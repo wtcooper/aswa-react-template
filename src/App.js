5da732bced0854482d5adeea2942dcff93572f8f
Wade Cooper <wadetcooper@gmail.com>
2022-12-16 07:03:38 -0500

    simple login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import NavBar from 'common/navbar/navbar';
 
 function App() {
   const [data, setData] = useState('default');
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  async function getUserInfo() {
+    try {
+
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        
+        if(clientPrincipal){
+          setUser(clientPrincipal);
+          userHasAuthenticated(true);
+          console.log(`clientPrincipal = ${JSON.stringify(clientPrincipal)}`);
+        } 
+        
+    } catch (error) {
+        console.error('No profile could be found ' + error?.message?.toString());
+    }
+};  
 
   useEffect(() => {
     (async function () {
-      const { text } = await( await fetch(`/api/test_fnx`)).json();
+      const { text } = await (await fetch(`/api/test_fnx`)).json();
       setData(text);
       console.log("FA text: ", text);
     })();
@@ -14,12 +39,29 @@ function App() {
   return (
     <>
 
-      <div>Test API</div>
+      {/* <div>Test API</div>
       <div>{data}</div>
-      <div></div>
+      <div></div> */}
 
-      <div>Test Login</div>
-      <a href="/.auth/login/github">Login</a>
+      <NavBar user={user}/>
+      <main className="column">
+        { isAuthenticated ? 
+        <div>
+          <div>Successfully logged in</div>
+          <div>{user.userDetails}</div>
+        </div>
+        : 
+        <div>Need to Login</div> }
+      </main>
+
+      {/* <div>Test Login</div>
+      <a href="/.auth/login/github">Login - Github</a>
+      <div></div>
+      <a href="/.auth/login/aadb2c">Login - AD B2C</a>
+      <div></div>
+      <a href="/.auth/logout">Log out</a> */}
+
+      
     </>
   );
 }
